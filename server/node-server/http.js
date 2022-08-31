ad47f9066ad67b0351051d809b7ac01ac354e4b9
lizhi <15501251520@163.com>
2022-08-31 12:08:46 +0800

    feat: server handle file

--- server/node-server/http.js
+++ server/node-server/http.js
@@ -1,5 +1,11 @@
 const http = require('http')
+const path = require('path')
+const multiparty = require('multiparty')
+const fse = require('fs-extra')
+
 const server = http.createServer()
+// 文件存储目录
+const UPLOAD_DIR = path.resolve(__dirname, '..', 'target')
 
 server.on('request', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -10,6 +16,26 @@ server.on('request', async (req, res) => {
     res.end()
     return
   }
+})
+
+const multipart = new multiparty.Form()
+
+multipart.parse(req, async (err, fields, files) => {
+  if (err) return
+
+  const [chunk] = files.chunk
+  const [hash] = fields.hash
+  const [filename] = fields.filename
+
+  // 创建文件夹用于临时存储 chunk
+  const chunDir = path.resolve(UPLOAD_DIR, 'chunkDir' + filename)
+
+  if (!fse.existsSync(chunDir)) {
+    await fse.mkdirs(chunDir)
+  }
+
+  await fse.move(chunk.path, `${chunDir}/${hash}`)
+  res.end('received file chunk')
 })
 
 server.listen(3000, () => console.log('listening port 3000'))
