91da641782399b9833e813c1088bf2a7d1b03e58
lizhi <15501251520@163.com>
2022-09-07 12:02:24 +0800

    feat: client progress handler

--- client/common/http.js
+++ client/common/http.js
@@ -1,6 +1,8 @@
-const request = ({ url, method = 'post', data, headers = {} }) => {
+const request = ({ url, method = 'post', data, headers = {}, onProgress = (e) => e }) => {
   return new Promise((resolve) => {
     const xhr = new XMLHttpRequest()
+
+    xhr.upload.onprogress = onProgress
     xhr.open(method, url)
     Object.keys(headers).forEach((key) => xhr.setRequestHeader(key, headers[key]))
     xhr.send(data)
--- client/vue3-client/src/App.vue
+++ client/vue3-client/src/App.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref, computed, watch } from 'vue'
 import request from '../../common/http.js'
 
 const SIZE = 10 * 1024 * 1024
 const container = reactive({ file: null })
 let data = reactive<any[]>([])
+let fakeUploadPercentage = ref(0)
+
+const uploadPercentage = computed(() => {
+  if (!container.file || !data.length) return 0
+
+  const loaded = data.map((item) => item.size * item.percentage).reduce((acc, cur) => acc + cur)
+
+  return parseInt((loaded / container.file?.size).toFixed(2))
+})
+
+watch(uploadPercentage, (now) => {
+  console.log('=== now ===', now)
+  if (now > fakeUploadPercentage.value) {
+    fakeUploadPercentage.value = now
+  }
+})
 
 // 保存文件
-function handleFileChange(e) {
+function handleFileChange(e: { target: { files: [any] } }) {
   const [file] = e.target.files
 
   if (!file) return
@@ -24,7 +40,9 @@ async function handleUpload() {
 
   data = fileChunkList.map(({ file }, index) => ({
     chunk: file,
+    index,
     hash: container.file?.name + '-' + index,
+    percentage: 0,
   }))
 
   console.log('=== data ===', data)
@@ -32,7 +50,7 @@ async function handleUpload() {
 }
 
 // 生成文件切片
-function createFileChunk(file, size = SIZE) {
+function createFileChunk(file: any, size = SIZE) {
   const fileChunkList = []
   let cur = 0
 
@@ -57,22 +75,32 @@ async function mergeRequest() {
     }),
   })
 }
+
+// 每个子切片监听上传进度事件
+function createProgressHandler(item: { percentage: number }) {
+  return (e: { loaded: number; total: number }) => {
+    console.log(item, e.loaded)
+    item.percentage = parseInt(String((e.loaded / e.total) * 100))
+  }
+}
+
 // 上传切片
 async function uploadChunks() {
   const requestList = data
-    .map(({ chunk, hash }) => {
+    .map(({ chunk, hash, index }) => {
       const formData = new FormData()
 
       formData.append('chunk', chunk)
       formData.append('hash', hash)
       formData.append('filename', container.file?.name)
 
-      return { formData }
+      return { formData, index }
     })
-    .map(({ formData }) => {
+    .map(({ formData, index }) => {
       request({
         url: 'http://localhost:3000',
         data: formData,
+        onProgress: createProgressHandler(data[index]),
       })
     })
 
@@ -91,6 +119,10 @@ async function handleMerge() {
   <input type="file" @change="handleFileChange" />
   <el-button type="success" @click="handleUpload">点击上传</el-button>
   <el-button type="success" @click="handleMerge">合并文件</el-button>
+  <div>
+    <div>percentage</div>
+    <el-progress :percentage="uploadPercentage"></el-progress>
+  </div>
   <RouterView />
 </template>
 
